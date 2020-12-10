647a2cb16390d8e8a843d0e30554dae693fa2741
derekalia <derekalia@me.com>
2020-12-10 14:51:42 -0800

    new exchange

--- build/constants.js
+++ build/constants.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.registryHolder = exports.fsWalletAddress = exports.stableTokenAddress = exports.assetTokenAddress = exports.exchangeAddress = exports.NETWORK = exports.ONE_ETHER = exports.CONTRACT_CALL_ENDPOINT = exports.WEBSOCKET_ENDPOINT = exports.MESSAGE_PROCESSOR_API_ENDPOINT = void 0;
-exports.MESSAGE_PROCESSOR_API_ENDPOINT = "https://896isl1khc.execute-api.us-east-1.amazonaws.com/Prod/api/v1";
+exports.MESSAGE_PROCESSOR_API_ENDPOINT = "https://q0kfejprdk.execute-api.us-east-1.amazonaws.com/Prod/api/v1";
 exports.WEBSOCKET_ENDPOINT = "wss://8a8hwmt919.execute-api.us-east-1.amazonaws.com/Dev";
 exports.CONTRACT_CALL_ENDPOINT = `${exports.MESSAGE_PROCESSOR_API_ENDPOINT}/contractcall`;
 exports.ONE_ETHER = "1000000000000000000";
 exports.NETWORK = "kovan";
-exports.exchangeAddress = "0xd34b0e37f3cb3c744f8ee3a5079a21f931fdfdac";
+exports.exchangeAddress = "0x063590C1513f8D911228e3FC18e45E7d9509d77a";
 exports.assetTokenAddress = "0x208FC7c199f53AC5e6c62c8021593073F84316d4";
 exports.stableTokenAddress = "0x20a355DB06eff6E44841EAe9eDAbCfAdb5Cb2fD5";
-exports.fsWalletAddress = "0xd28d69bde1ee91ff75cde3180a2cb531afd26a6e";
-exports.registryHolder = "0xDcf0424b6c0A7aF3133e04D1A31C0dac0408e530";
+exports.fsWalletAddress = "0x6aA9b2Bf2A846e41954f3aFDa4800bC623B1Bd28";
+exports.registryHolder = "0x4962b90E6A945bc9bB5b782bC0C9f3822506d175";
--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,12 @@
 export const MESSAGE_PROCESSOR_API_ENDPOINT =
-  "https://896isl1khc.execute-api.us-east-1.amazonaws.com/Prod/api/v1";
+  "https://q0kfejprdk.execute-api.us-east-1.amazonaws.com/Prod/api/v1";
 export const WEBSOCKET_ENDPOINT =
   "wss://8a8hwmt919.execute-api.us-east-1.amazonaws.com/Dev";
 export const CONTRACT_CALL_ENDPOINT = `${MESSAGE_PROCESSOR_API_ENDPOINT}/contractcall`;
 export const ONE_ETHER = "1000000000000000000";
 export const NETWORK = "kovan";
-export const exchangeAddress = "0xd34b0e37f3cb3c744f8ee3a5079a21f931fdfdac";
+export const exchangeAddress = "0x063590C1513f8D911228e3FC18e45E7d9509d77a";
 export const assetTokenAddress = "0x208FC7c199f53AC5e6c62c8021593073F84316d4";
 export const stableTokenAddress = "0x20a355DB06eff6E44841EAe9eDAbCfAdb5Cb2fD5";
-export const fsWalletAddress = "0xd28d69bde1ee91ff75cde3180a2cb531afd26a6e";
-export const registryHolder = "0xDcf0424b6c0A7aF3133e04D1A31C0dac0408e530";
+export const fsWalletAddress = "0x6aA9b2Bf2A846e41954f3aFDa4800bC623B1Bd28";
+export const registryHolder = "0x4962b90E6A945bc9bB5b782bC0C9f3822506d175";
